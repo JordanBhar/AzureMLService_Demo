dd270558e210da49018df9d23903842ad8a66cfc
Jordan Bhar <jordan.bhar@icloud.com>
2025-02-21 11:19:28 -0500

    Re-Initalized Code on my mac, also started changing to function_app.py to forward data to AI vision

--- ML Demo/function_app.py
+++ ML Demo/function_app.py
@@ -1,19 +1,19 @@
 import azure.functions as func
+import requests
+import json
 import logging
 
-app = func.FunctionApp()
+# Azure Vision API Credentials
+VISION_ENDPOINT = "https://<your-region>.api.cognitive.microsoft.com/vision/v3.2/analyze"
+VISION_KEY = "<YOUR_AZURE_VISION_API_KEY>"
 
-@app.event_hub_message_trigger(
-    arg_name="azeventhub",
-    event_hub_name="alphabet-topic",
-    connection="CRAVEventHub_RootManageSharedAccessKey_EVENTHUB"
-)
-def ImageHub_ImageProduced_trigger(azeventhub: func.EventHubEvent):
-    logging.info("✅ EventHub Triggered! Processing event...")
-    logging.info("Received event: %s", azeventhub.get_body().decode('utf-8'))
+# Headers for Azure Vision API
+HEADERS = {
+    "Ocp-Apim-Subscription-Key": VISION_KEY,
+    "Content-Type": "application/octet-stream"
+}
 
-
-    #Note for Jordan
-    #Make sure to run npm install azureite in the terminal to install the azureite package so that the code can run locally
-    #Also make sure to have the azureite package installed in the requirements.txt file
+# Azure Function Triggered by Event Hub
+def main(event: func.EventHubEvent):
+    
     
